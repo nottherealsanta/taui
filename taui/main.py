1d70bff271b5814fa74b9e56aed1e53138a968eb
nottherealsanta <ucsantosh@gmail.com>
2026-05-18 17:34:48 +0530

    Bump version to 0.5

--- taui/main.py
+++ taui/main.py
@@ -93,7 +93,7 @@ def main(argv: list[str] | None = None) -> None:
     parsed = parse_args(argv)
 
     if parsed.pop("version", False):
-        print("taui 0.4")
+        print("taui 0.5")
         return
 
     if parsed.pop("login", False):
